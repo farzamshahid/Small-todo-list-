891523db16abafa88316f70123d8de52a95268d1
farzamshahid <farzamshahid6@gmail.com>
2024-05-21 12:11:48 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Flex,
+  Text,
+  Input,
+  Button,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Checkbox,
+  IconButton
+} from '@chakra-ui/react'
+import { DeleteIcon } from '@chakra-ui/icons'
 
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [newTask, setNewTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const addTask = e => {
+    e.preventDefault()
+
+    if (newTask.length > 0) {
+      setTasks(prevState => [
+        ...prevState,
+        { text: newTask, isChecked: false }
+      ])
+      setNewTask('')
+    }
+  }
+
+  const updateTask = (index, checked) => {
+    let newTasks = [...tasks]
+    newTasks[index].isChecked = checked
+    setTasks(newTasks)
+  }
+
+  const removeTask = index => {
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    setTasks(newTasks)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Flex w='100%' h='100vh'>
+        <Flex w='100%' flexDir='column' ml='20%' mt='5%' mr='20%' color='white'>
+          <Text fontWeight='700' fontSize={30}>Tasks</Text>
+          <form onSubmit={addTask}>
+            <Flex mt='2%'>
+              <Input value={newTask} onChange={e => setNewTask(e.target.value)} variant='flushed' placeholder='Add task' w='50%' />
+              <Button onClick={addTask} ml={5} bg='blue.400'>Add Task</Button>
+            </Flex>
+          </form>
+          <Tabs mt='2%' w='100%'>
+            <TabList>
+              <Tab>Incomplete Tasks</Tab>
+              <Tab>Completed Tasks</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                {tasks.map((task, index) => (
+                  !task.isChecked ? <TaskItem removeTask={removeTask} updateTask={updateTask} key={index} task={task} index={index} /> : null
+                ))}
+              </TabPanel>
+              <TabPanel>
+                {tasks.map((task, index) => (
+                  task.isChecked ? <TaskItem removeTask={removeTask} updateTask={updateTask} key={index} task={task} index={index} /> : null
+                ))}
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Flex>
+      </Flex>
+    </>
+  )
 }
 
-export default App;
+const TaskItem = ({ task, index, updateTask, removeTask }) => {
+  return (
+    <Checkbox onChange={e => updateTask(index, e.target.checked)} colorScheme='green' mb={10} w='100%' flexDir='row' isChecked={task.isChecked}>
+      <Flex w='100%' flexDir='row'>
+        <Text color='white' alignSelf='center'>{task.text}</Text>
+        <IconButton onClick={() => removeTask(index)} bg='red.600' pos='absolute' right={0} icon={<DeleteIcon />} />
+      </Flex>
+    </Checkbox>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const config = { initialColorMode: 'dark' }
+const theme = extendTheme({ config })
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
